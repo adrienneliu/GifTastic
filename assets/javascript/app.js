3ac096267206428aa6f9c3d0364fae9efcd3d30b
Adrienne <iliu.adrienne@gmail.com>
2019-06-06 01:08:26 -0700

    fixed the foodshown array to show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,66 @@
-$(document).submit(function () {
-    event.preventDefault();
+var foodshown = ["spaghetti", "fried chicken", "sushi", "pizza", "carrot", "burrito", "hot dog", "spring roll", "dim sum", "lumpia"];
 
 
-    var food = ["spaghetti", "fried chicken", "sushi"];
+function renderButtons() {
+    $("#foodbuttons").empty();
+
+    for (var i = 0; i < foodshown.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("foodSearch");
+        newButton.attr("data-name", foodshown[i]);
+        newButton.text(foodshown[i]);
+        $("#foodbuttons").append(newButton);
+        console.log('renderButtons');
+    }
+
+}
+
+$("#add-food").on("click", function (event) {
+    var foodSearch = $("#foodSearch").val().trim();
+    foodshown.push(foodSearch);
+    renderButtons();
+    console.log(foodval);
+})
 
     function displayFood() {
-  
-    var foodSearch; 
-//Storing the value in the search input
-    var foodSearch = $("#foodSearch").val();
 
-    console.log(foodSearch);
+        var foodSearch;
+        //Storing the value in the search input
+        var foodSearch = $(this).attr("data-name");
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + foodSearch + "&api_key=xldevyUFda5jgS3659tn5rBHaORqZjBi&limit=5";
-    console.log(queryURL);
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + foodSearch + "&api_key=xldevyUFda5jgS3659tn5rBHaORqZjBi&limit=5";
+        //console.log(queryURL);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-        var results = response.data; 
-        for (var i = 0; i<foodSearch.length; i++) {
-        
-        var foodDiv = $("<div>")
-        var foodP = $("<p>")
-        var foodURL = results[i].images.fixed_height.url;
-        var foodImage = $("<img>"); 
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
 
-        foodImage.attr("src", foodURL);
-        foodImage.attr("alt", "food")
-        
-        foodDiv.append(foodP);
-        foodDiv.append(foodImage);
-        $(".foodImage").prepend(foodDiv);
-    }
-    }) 
-    }
+                var foodDiv = $("<div>")
+                var foodP = $("<p>")
+                var foodURL = results[i].images.fixed_height.url;
+                var foodImage = $("<img>");
 
-    function renderButtons() {
-        $("#foodbuttons").empty();
+                foodImage.attr("src", foodURL);
+                foodImage.attr("alt", "food")
 
-        for (var i = 0; i<foodSearch.length; i++) {
-        var newButton = $("<button>");
-        newButton.addClass("food-button");
-        newButton.attr("data-name", foodSearch[i]);
-        newButton.text(foodSearch[i]);
-        $("#foodbuttons").append(newButton);
-        }
-
+                foodDiv.append(foodP);
+                foodDiv.append(foodImage);
+                $(".foodImage").prepend(foodDiv);
+            }
+        })
     }
 
 
+$(document).on("click", ".foodSearch", displayFood);
+renderButtons();
 
-    renderButtons();
-    displayFood();
-});
 
-//dynamically create buttons 
+//dynamically create buttons
 
 // have a search bar with a value in it, so that the value can be retrieved
 
@@ -79,3 +81,12 @@ $(document).submit(function () {
 //call on giphy to pull images
 
 //stle page
+
+
+
+
+
+
+        // $(document).on("click", "food-button", displayFood);
+        //renderButtons();
+        //displayFood();
