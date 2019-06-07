843ab930f1bbba4c1d35ff21be36265eed22c125
Adrienne <iliu.adrienne@gmail.com>
2019-06-06 18:31:36 -0700

    add rating and background

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var foodshown = ["spaghetti", "fried chicken", "sushi", "pizza", "carrot", "burrito", "hot dog", "spring roll", "dim sum", "lumpia"];
+var foodshown = ["spaghetti", "fried chicken", "sushi", "pizza", "carrot", "burrito", "hot dog", "spring roll", "dim sum", "lumpia", "salad", "chili", "samosa", "lasagna", "ramen", "milk tea", "bacon", "udon", "wine", "potato", "french fries", "hamburger", "braised meat", "pho", "meatloaf", "sandwich"];
 
 
 function renderButtons() {
@@ -28,7 +28,7 @@ $("#add-food").on("click", function (event) {
         //Storing the value in the search input
         var foodSearch = $(this).attr("data-name");
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + foodSearch + "&api_key=xldevyUFda5jgS3659tn5rBHaORqZjBi&limit=5";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + foodSearch + "&api_key=xldevyUFda5jgS3659tn5rBHaORqZjBi&limit=10";
         //console.log(queryURL);
 
         $.ajax({
@@ -40,17 +40,23 @@ $("#add-food").on("click", function (event) {
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
 
-                var foodDiv = $("<div>")
-                var foodP = $("<p>")
+                              
+
+                var foodDiv = $("<div>");
+                var foodP = $("<p>");
                 var foodURL = results[i].images.fixed_height.url;
                 var foodImage = $("<img>");
 
                 foodImage.attr("src", foodURL);
-                foodImage.attr("alt", "food")
+                foodImage.attr("alt", "food");
 
                 foodDiv.append(foodP);
                 foodDiv.append(foodImage);
                 $(".foodImage").prepend(foodDiv);
+
+                var rating = results[i].rating;
+                var ratingText = $("<p>").text("Rating: " + rating);
+                foodDiv.prepend(ratingText);
             }
         })
     }
